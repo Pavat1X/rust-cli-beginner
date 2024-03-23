876d6e3a634c656d10b3280ca062fdadab766bfb
Pavat Pichetsin <pavatpichetsin@outlook.com>
2024-03-23 12:47:49 +0000

    I implemented a workin g version of my very very simple idea for a CLI. I initiated a struct for CL argum ents and added the clap package to my project. I wrote code that would print out a  message when faced with a specific CL argument

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use clap::Parser;
+#[derive(Parser)]
+struct Cli {
+    pattern: String,
+}
 fn main() {
-    println!("Hello, world!");
+    let ans: &str = "mentalsupport";
+    let args: Cli= Cli::parse();
+    if &args.pattern == ans {
+        println!("Your dissertation has been submitted already")
+    }
 }
